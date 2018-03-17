510a59c631adedb29e7af3772184553cf0caf54a
Andrew Abbott <mirrexagon@mirrexagon.com>
2018-03-17 12:47:29 +1100

    Add shell.nix
    For all you NixOS users out there!
    
    ...just me.

---
+++ shell.nix
@@ -0,0 +1,29 @@
+with import <nixpkgs> {};
+
+let
+  libPath = lib.makeLibraryPath [
+    openssl
+  ];
+in stdenv.mkDerivation {
+  name = "steelconnect-dialogflow-env";
+
+  buildInputs = (with python27Packages; [
+    python
+    pip
+  ]) ++ [
+    pkgconfig
+
+    openssl
+  ];
+
+  PYTHONPATH = (builtins.getEnv "HOME") + "/.local/lib/python2.7/site-packages/";
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="${libPath}:$LD_LIBRARY_PATH"
+  '';
+
+  # Run `source installDeps`
+  installDeps = ''
+    pip install -U --user -r requirements-dev.txt
+  '';
+}
